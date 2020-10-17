4744f69c17c5ffd6f802395f7cc8172777c35d2d
Vishesh Singh <VisheshSingh16@gmail.com>
2020-10-16 21:00:17 -0500

    Making APIrequests 🎀

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Articles = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+      const data = await res.json();
+      setArticles(data);
+    }, 5000);
+  }, []);
+
   return (
     <div className='articles'>
       <h2>Articles</h2>
+
+      {articles &&
+        articles.map((article) => (
+          <article key={article.id}>
+            <h3>{article.title}</h3>
+            <p>{article.body}</p>
+          </article>
+        ))}
+
+      {articles.length === 0 && <p>Loading...</p>}
     </div>
   );
 };
--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const User = () => {
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/users/1`);
+      const data = await res.json();
+      setProfile(data);
+    }, 5000);
+  }, []);
+
   return (
     <div className='User'>
       <h2>User details</h2>
+
+      {profile && (
+        <div className='profile'>
+          <h3>{profile.username}</h3>
+          <p>{profile.email}</p>
+          <p>{profile.website}</p>
+        </div>
+      )}
+
+      {!profile && <p>Loading...</p>}
     </div>
   );
 };
